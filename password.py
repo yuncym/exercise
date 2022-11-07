e9910f2a763d98a83b6299ff5c09a23a5ab7913d
yuncym <yuncym@gmail.com>
2022-11-08 00:46:13 +0800

    change file - password.py

--- password.py
+++ password.py
@@ -3,7 +3,7 @@
 num = 3
 password = 'a123456'
 
-while True:
+while num > 0:
 	input_pwd = input('請輸入密碼: ')
 	if input_pwd == password:
 		print('輸入正確')
@@ -12,6 +12,4 @@ while True:
 		num = num - 1
 		print('輸入錯誤, 請重複輸入: ')
 		print('您還有', num, '次機會')
-		if num == 0:
-			print ('已失敗三次, 登入失敗')
-			break
+		
